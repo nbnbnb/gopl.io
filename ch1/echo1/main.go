2f756b16a23b0af29a5695a4c250bbc1b1318d1a
JJZhang <seoul-arm@zhangjin.tk>
2023-04-28 12:44:11 +0800

    echo

--- ch1/echo1/main.go
+++ ch1/echo1/main.go
@@ -1,10 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 4.
-//!+
-
-// Echo1 prints its command-line arguments.
 package main
 
 import (
@@ -13,12 +6,13 @@ import (
 )
 
 func main() {
+
 	var s, sep string
+
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
+
 	fmt.Println(s)
 }
-
-//!-
