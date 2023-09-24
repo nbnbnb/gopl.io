dddd74a0b5daadad927e737f53d0c3fa9ddbd293
JJZhang <dashan1010zj@163.com>
2023-09-24 15:40:31 +0800

    cc

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -1,9 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 16.
-//!+
-
 // Fetch prints the content found at each specified URL.
 package main
 
@@ -15,14 +9,20 @@ import (
 )
 
 func main() {
+	if len(os.Args) == 1 {
+		os.Args = append(os.Args, "https://www.baidu.com/", "https://www.sina.com.cn/")
+	}
+
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+
 		b, err := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
+
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
@@ -30,5 +30,3 @@ func main() {
 		fmt.Printf("%s", b)
 	}
 }
-
-//!-
