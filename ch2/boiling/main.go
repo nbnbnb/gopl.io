a0e10b4a52075f850b0ee47ed100551dfbe66509
JJZhang <dashan1010zj@163.com>
2023-09-24 18:35:00 +0800

    cc

--- ch2/boiling/main.go
+++ ch2/boiling/main.go
@@ -1,9 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 29.
-//!+
-
 // Boiling prints the boiling point of water.
 package main
 
@@ -12,11 +6,9 @@ import "fmt"
 const boilingF = 212.0
 
 func main() {
-	var f = boilingF
-	var c = (f - 32) * 5 / 9
+	f := boilingF
+	c := (f - 32) * 5 / 9
 	fmt.Printf("boiling point = %g°F or %g°C\n", f, c)
 	// Output:
 	// boiling point = 212°F or 100°C
 }
-
-//!-
--- ch2/echo4/main.go
+++ ch2/echo4/main.go
@@ -1,9 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 33.
-//!+
-
 // Echo4 prints its command-line arguments.
 package main
 
@@ -13,15 +7,17 @@ import (
 	"strings"
 )
 
-var n = flag.Bool("n", false, "omit trailing newline")
-var sep = flag.String("s", " ", "separator")
+var (
+	n   = flag.Bool("n", false, "omit trailing newline")
+	sep = flag.String("s", " ", "separator")
+)
 
 func main() {
+	// 当程序运行时，必须在使用标志参数对应的变量之前先调用 flag.Parse 函数
+	// 用于更新每个标志参数对应变量的值（之前是默认值）
 	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
 		fmt.Println()
 	}
 }
-
-//!-
