75318cd3d90a2be066d64bfa1a9093d222c0c44e
JJZhang <dashan1010zj@163.com>
2023-10-15 13:01:07 +0800

    cc

--- ch3/basename1/main.go
+++ ch3/basename1/main.go
@@ -1,9 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 72.
-
-// Basename1 reads file names from stdin and prints the base name of each one.
+// 从标准输入读入文件名，然后将其转换位基本文件名（去掉.和后缀）输出
 package main
 
 import (
@@ -12,33 +7,47 @@ import (
 	"os"
 )
 
+/*
+
+测试方式 - CMD 中执行
+
+go run .\main.go
+a.txt
+回车
+b.iso
+回车
+c.java
+回车
+*/
+
 func main() {
 	input := bufio.NewScanner(os.Stdin)
+	// Scan每次读取一行，去掉换行符，返回 bool值
+	//  如果读到了内容，返回 true，否则返回 false
 	for input.Scan() {
 		fmt.Println(basename(input.Text()))
 	}
-	// NOTE: ignoring potential errors from input.Err()
 }
 
-//!+
 // basename removes directory components and a .suffix.
-// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
+// 示例 a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
 func basename(s string) string {
 	// Discard last '/' and everything before.
 	for i := len(s) - 1; i >= 0; i-- {
 		if s[i] == '/' {
+			// 去掉最后一个 '/' 和之前的所有字符
 			s = s[i+1:]
 			break
 		}
 	}
+
 	// Preserve everything before last '.'.
 	for i := len(s) - 1; i >= 0; i-- {
 		if s[i] == '.' {
+			// 去掉最后一个 '.' 和之后的所有字符
 			s = s[:i]
 			break
 		}
 	}
 	return s
 }
-
-//!-
