176a783f1aacf48402e8f2286c50e6c3af1bad86
JJZhang <dashan1010zj@163.com>
2023-10-15 13:03:01 +0800

    cc

--- ch3/basename2/main.go
+++ ch3/basename2/main.go
@@ -1,9 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 72.
-
-// Basename2 reads file names from stdin and prints the base name of each one.
+// 从标准输入读入文件名，然后将其转换位基本文件名（去掉.和后缀）输出
 package main
 
 import (
@@ -18,19 +13,21 @@ func main() {
 	for input.Scan() {
 		fmt.Println(basename(input.Text()))
 	}
-	// NOTE: ignoring potential errors from input.Err()
 }
 
+// 另一种实现
 // basename removes directory components and a trailing .suffix.
 // e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
-//!+
 func basename(s string) string {
-	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	// -1 if "/" not found
+	slash := strings.LastIndex(s, "/")
+
+	// 去除最后一个 '/' 和之前的所有字符
 	s = s[slash+1:]
+
 	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		// 去除最后一个 '.' 和之后的所有字符
 		s = s[:dot]
 	}
 	return s
 }
-
-//!-
